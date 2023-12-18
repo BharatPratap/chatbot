88fcd2b0043cc258ffef9b27e6929e9539230e1b
BPratap <54697708+BharatPratap@users.noreply.github.com>
2023-12-18 12:29:27 +0530

    Add files via upload

---
+++ db.js
@@ -0,0 +1,29 @@
+// db.js
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const uri = process.env.MONGO_URI;
+
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function connect() {
+    try {
+        await client.connect();
+        console.log('Connected to MongoDB');
+    } catch (error) {
+        console.error('Error connecting to MongoDB:', error);
+    }
+}
+
+function getClient() {
+    return client;
+}
+
+module.exports = { connect, getClient };
